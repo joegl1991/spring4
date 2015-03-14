de7ca471c42c48d0501d443005091fd5e9ae59c0
Jose Diaz <jose.diaz@joedayz.pe>
2015-03-14 13:11:17 -0500

    1.0 sin spring matar al dragonañadido

--- Reyes/src/main/java/com/mycompany/reyes/MatarDragonAventura.java
+++ Reyes/src/main/java/com/mycompany/reyes/MatarDragonAventura.java
@@ -1,10 +1,19 @@
 package com.mycompany.reyes;
 
+import java.io.PrintStream;
+
 public class MatarDragonAventura implements Aventura {
 
+	private PrintStream stream;
+	
+	public MatarDragonAventura(PrintStream stream){
+		this.stream = stream;
+	}
+	
 	@Override
 	public void embarcar() {
-		// TODO Auto-generated method stub
+		stream.println("Embarcandome en la aventura"
+				+ " para matar al dragón.");
 
 	}
 
